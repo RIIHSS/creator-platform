2f7439efbea4c196155cd3acf17e990e3c59b4c5
Risshi <risshirraj@gmail.com>
2026-05-03 19:17:39 +0530

    adding update and delete options

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { protect } from '../middleware/auth.js';
-import { createPost, getPosts } from '../controllers/postController.js';
+import { createPost, getPosts, deletePost, getPostById, updatePost } from '../controllers/postController.js';
 
 const router = express.Router();
 
 // Both routes require authentication
 router.post('/', protect, createPost);
 router.get('/', protect, getPosts);
+router.delete('/:id', protect, deletePost);
+router.put('/:id', protect, updatePost);
+router.get('/:id', protect, getPostById);
 
 export default router;
+
